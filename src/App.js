efdc67f84c17ffcf0c917d85a0a618f9d6997bba
Bathri <115721166+BadhriBBN@users.noreply.github.com>
2023-01-23 22:23:23 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        
+          <div className='title-card'>
+        <div className='category'>FREE</div>
+        <div className='heading'>$0<span className='heading-title'>/month</span></div>
+        </div><br/>
+       <hr/>  
+       <div className='symbol'>✔ <span>Single User</span></div>
+       <br/>
+       <div className='symbol'>✔ <span>5GB Storage</span></div>
+       <br/>
+       <div className='symbol'>✔ <span>Unlimited Public Projects</span></div>
+       <br/>
+       <div className='symbol'>✔ <span>Community Access</span></div>
+       <br/>
+       <div className='symbol'><span className='text-muted'> ✖ Unlimited Private Projects</span></div>
+       <br/>
+       <div className='symbol'><span className='text-muted'> ✖ Dedicated Phone Support</span></div>
+       <br/>
+       <div className='symbol'><span className='text-muted'> ✖ Free subdomin</span></div>
+       <br/>
+       <div className='symbol'><span className='text-muted'> ✖ Monthly Status Reports</span></div><br/>
+
+       <button className='btn-button'>BUTTON</button>
+       </div>
+       <div className='container'>
+        <div className='title-card'>
+        <div className='category'>PRO</div>
+        <div className='heading'>$9<span className='heading-title'>/month</span></div>
+        </div><br/>
+       <hr/>
+      
+       <div className='symbol'>✔ <span><b>5 User</b></span></div>
+       <br/>
+       <div className='symbol'>✔ <span>50GB Storage</span></div>
+       <br/>
+       <div className='symbol'>✔ <span>Unlimited Public Projects</span></div>
+       <br/>
+       <div className='symbol'>✔ <span>Community Access</span></div>
+       <br/>
+       <div className='symbol'>✔ Unlimited Private Projects</div>
+       <br/>
+       <div className='symbol'>✔ Dedicated Phone Support</div>
+       <br/>
+       <div className='symbol'>✔ Free subdomin</div>
+       <br/>
+       <div className='symbol'><span className='text-muted'> ✖ Monthly Status Reports</span></div><br/>
+
+       <button className='btn-button'>BUTTON</button>     
+       </div>
+       <div className='container'>
+        <div className='title-card'>
+        <div className='category'>FREE</div>
+        <div className='heading'>$0<span className='heading-title'>/month</span></div>
+        </div><br/>
+       <hr/>
+       <div className='symbol'>✔ <span><b>Unlimited User</b></span></div>
+       <br/>
+       <div className='symbol'>✔ <span>150GB Storage</span></div>
+       <br/>
+       <div className='symbol'>✔ <span>Unlimited Public Projects</span></div>
+       <br/>
+       <div className='symbol'>✔ <span>Community Access</span></div>
+       <br/>
+       <div className='symbol'>✔ Unlimited Private Projects</div>
+       <br/>
+       <div className='symbol'>✔ Dedicated Phone Support</div>
+       <br/>
+       <div className='symbol'>✔ <b>Unlimited</b>Free subdomin</div>
+       <br/>
+       <div className='symbol'>✔ Monthly Status Reports</div><br/>
+       
+
+       <button className='btn-button'>BUTTON</button>
+        
+       </div>
+    </div>
+    
+    
+  );
+}
+
+export default App;
+
